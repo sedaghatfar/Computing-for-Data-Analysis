00f5f08868ccc67e49dc45579c0de9344db4089c
sedaghatfar <sedaghatfar@gmail.com>
2014-01-26 16:43:51 -0500

    Update Best.R

--- Best.R
+++ Best.R
@@ -1,27 +1,24 @@
 ## Programming Assignment 3: Best.R
 
 best <- function(state, outcome) {
-  oc <- read.csv('outcome-of-care-measures.csv', colClasses = 'character')
-  oc[,11] <- suppressWarnings(as.numeric(oc[,11]))
-  oc[,17] <- suppressWarnings(as.numeric(oc[,17]))
-  oc[,23] <- suppressWarnings(as.numeric(oc[,23]))
+  outcomes=getOutcomes()
   
-  #create a list of states for verification
-  states <- unique(oc$State)
-  #make a list of outcomes to check for
-  conditions <- c('heart attack', 'heart failure', 'pneumonia')
-  if (!state %in% states) { stop('invalid state') }
-  if (!outcome %in% conditions) { stop('invalid outcome') }
   
-  #make a subset, just for that state
-  soc <- oc[grep(state, oc$State, ignore.case=T),]
+  ## Read outcome data
+  data=getData(state)
   
-  #pick a column to select based on the outcome
-  if (outcome == 'heart attack' ) { selector <- 11 }
-  if (outcome == 'heart failure' ) { selector <- 17 }
-  if (outcome == 'pneumonia' ) { selector <- 23 }
+  #Prepare data  
+  data=prepareData(data)
   
-  sorted <- soc[order(soc[,selector],soc[,2]),]
-  #sorted <- na.omit(sorted)
-  sorted[1,2]
+  data = data[data$State == state,]
+  
+  # clean up NA
+  data=cleanNA(data)
+  
+  idx=getIdx(outcome)
+  
+  t_data <- data[!is.na(data[,idx]),]
+  return(t_data[which.min(t_data[,idx]),2])
+  
+  
 }
