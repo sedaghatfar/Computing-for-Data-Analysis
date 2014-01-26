38194992b89c7fd69570eb9303b0a9f89ed1cb76
sedaghatfar <sedaghatfar@gmail.com>
2014-01-26 17:55:26 -0500

    Update rankhospital.R

--- rankhospital.R
+++ rankhospital.R
@@ -1,63 +1,30 @@
-rankhospital <- function(state, outcome, num = "best") {
-  ## Read outcome data
-  readFile = read.csv("outcome-of-care-measures.csv", colClasses = "character")
-  State = readFile[, "State"]
-  Hospital.Name = readFile[, "Hospital.Name"]
-  
-  readFile[, 11] = as.numeric(readFile[, 11])
-  Heart.Attack = readFile[, 11]
-  
-  readFile[, 17] = as.numeric(readFile[, 17])
-  Heart.Failure = readFile[, 17]
-  
-  readFile[, 23] = as.numeric(readFile[, 23])
-  Pneumonia = readFile[, 23]
-  
-  my.data = data.frame(Heart.Attack, Heart.Failure, Pneumonia, State, Hospital.Name)
-  
-  
-  bool.state = my.data[, "State"] == state
-  state.data = my.data[bool.state, ]
-  state.data = na.omit(state.data)
-  
-  ## Check that state and outcome are valid
-  if(nrow(state.data) == 0){
-    stop("Invalid state")
+rankhospital <- function(state, outcome, num = "best" ){
+  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  if (!state %in% data$State) {
+    stop("invalid state")
   }
-  
-  ## Return hospital name in that state with the given rank
-  ## 30-day death rate
-  if(outcome == "heart attack"){
-    ranked.state.data = state.data[ do.call(order, state.data), ]
-    
-    
-  }else if(outcome == "heart failure"){
-    state.data$Heart.Attack <- NULL
-    ranked.state.data = state.data[ do.call(order, state.data), ]
-    
-  }else if(outcome == "pneumonia"){
-    state.data$Heart.Attack <- NULL
-    state.data$Heart.Failure <- NULL
-    ranked.state.data = state.data[ do.call(order, state.data), ]
-    
+  data <- data[data$State==state,]
+  if (outcome == 'heart attack') {
+    death <- as.numeric(data[,11])
+  } else if (outcome == 'heart failure') {
+    death <- as.numeric(data[,17])
+  } else if (outcome == 'pneumonia') {
+    death <- as.numeric(data[,23])
   } else {
-    stop("Invalid outcome")
+    stop("invalid outcome")
   }
   
-  if(num == "best"){
-    rankedHospital = ranked.state.data[1, "Hospital.Name"]
-  } else if(num == "worst"){
-    worstRow = max(ranked.state.data[, 1])
-    rankedHospital = ranked.state.data[which(ranked.state.data[,1] == worstRow), "Hospital.Name"]
+  a <- rank(death, na.last=NA)
+  
+  if (num=="best") {
+    r <- 1
+  } else if (num =="worst") {
+    r <- length(a)
+  } else if (num <= length(a) ) {
+    r <- num
   } else {
-    num = as.integer(num)
-    if(typeof(num) == "integer" && nrow(ranked.state.data) >= num){
-      rankedHospital = ranked.state.data[num, "Hospital.Name"]
-    } else {
-      print(NA)
-    }
+    return(NA)
   }
   
-  rankedHospital = as.character(rankedHospital)
-  return(rankedHospital[1])
+  return(data$Hospital.Name[order(death, data$Hospital.Name)[r]])
 }
