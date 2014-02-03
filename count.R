630e411ea261f58a92e68aa8835764a4689eff0f
sedaghatfar <sedaghatfar@gmail.com>
2014-02-02 21:15:58 -0500

    Create count.R

---
+++ count.R
@@ -0,0 +1,24 @@
+count <- function(cause = NULL){
+  
+  ## Read "homicides.txt" data file
+  homidices <- readLines("homicides.txt")
+  
+  ## Check that "cause" is non-NULL; else throw error
+  if (is.null(cause)) stop("cause null")
+  
+  cause <- tolower(cause)
+  ## Check that specific "cause" is allowed; else throw error
+  causes <- c('asphyxiation', 'blunt force', 'other', 
+              'shooting', 'stabbing', 'unknown')
+  if (! cause %in% causes) stop('cause is not allowed')
+  
+  
+  
+  ## Extract causes of death
+  r <- regexec("<dd>Cause: (.*?)</dd>", homicides)
+  m <- regmatches(homicides, r)
+  res <- sapply(m, function(x) if (!is.na(x[2]))x[2])
+  res <- tolower(res)
+  ## Return integer containing count of homicides for that cause
+  length(res[res == cause])
+}
